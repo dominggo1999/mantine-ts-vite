eea6ea5b44e60a544c8d5ce15d095a26f25d3b1b
dominggo1999 <arnolddominggo19@gmail.com>
2022-10-21 16:29:59 +0700

    add more components

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -2,13 +2,17 @@ import type { GlobalProvider } from '@ladle/react';
 import { MantineProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
 
-export const Provider: GlobalProvider = ({ children, globalState }) => (
-  <MantineProvider
-    withGlobalStyles
-    withNormalizeCSS
-  >
-    <NotificationsProvider>
-      {children}
-    </NotificationsProvider>
-  </MantineProvider>
-);
+export const Provider: GlobalProvider = ({ children, globalState }) => {
+  return (
+
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{ colorScheme: globalState.theme as 'dark' | 'light' }}
+    >
+      <NotificationsProvider>
+        {children}
+      </NotificationsProvider>
+    </MantineProvider>
+  );
+};
