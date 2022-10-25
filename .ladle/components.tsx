2d710748a9e6cd465dd188c9f2714ac733c60e20
dominggo1999 <arnolddominggo19@gmail.com>
2022-10-25 23:02:22 +0700

    customize ladle

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -1,8 +1,26 @@
 import type { GlobalProvider } from '@ladle/react';
 import { MantineProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
+import { useHotkeys } from '@mantine/hooks';
+// eslint-disable-next-line import/no-unresolved
+import { useLadleContext, ActionType, ThemeState } from '@ladle/react';
+import './style.css';
 
 export const Provider: GlobalProvider = ({ children, globalState }) => {
+  const { dispatch } = useLadleContext();
+
+  useHotkeys([
+    ['ctrl+J', () => {
+      dispatch({
+        type: ActionType.UpdateTheme,
+        value:
+          globalState.theme === ThemeState.Dark
+            ? ThemeState.Light
+            : ThemeState.Dark,
+      });
+    }],
+  ]);
+
   return (
     <MantineProvider
       withGlobalStyles
